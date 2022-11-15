6cf6228c4534b9f97e1ac1dd40c37daeeab817c3
ryohakjae114 <ryohakjae114@gmail.com>
2022-11-15 15:50:59 +0900

    validatesの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,6 @@ class Item < ApplicationRecord
   validates :fat, presence: true, numericality: true
   validates :carb, presence: true, numericality: true
   validates :df, presence: true, numericality: true
-  validates :around_qty
   validates :unit, presence: true
   validates :weight, presence: true, numericality: true
 end
