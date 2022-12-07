5e0760771df3370fe19c32ebf1dcd1de3e97b09f
ryohakjae114 <ryohakjae114@gmail.com>
2022-12-07 17:02:12 +0900

    アイテムの検索機能を追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+2000.times do
+  Item.create(name: Faker::Food.ingredient, 
+              kcal: rand(201), protein: rand(201), fat: rand(201), carb: rand(201), df: rand(201),
+              around_qty: Faker::Lorem.sentence(word_count: 10), unit: "g", weight: 1)
+end
+
+2000.times do
+  cook_at =
+    if rand(2) == 0
+      "一般料理"
+    else
+      "外食・市販"
+    end
+  Recipe.create(name: Faker::Food.dish, cook_at: cook_at)
+end
